f3d9651d8c45061dab59c6ee67bd19d3482fd345
Nicky Irawan <niwasmala@gmail.com>
2021-11-11 21:03:37 +0700

    chore: Check if browser blocking javascript by calling alert

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,11 @@
 <script>
-	import { browser } from '$app/env';
 	import { onMount } from 'svelte';
 
 	let name = 'hello';
-	let isBrowser = false;
 
 	onMount(() => {
 		name = 'world';
-		isBrowser = true;
 	});
 </script>
 
 <h1>Welcome to {name}</h1>
-{browser} {isBrowser}
