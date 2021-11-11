139bac4eb6cd92bb88661f3223747559dd8e3872
Nicky Irawan <niwasmala@gmail.com>
2021-11-11 11:59:10 +0700

    chore(init): Init repository

---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { browser } from '$app/env';
+	import { onMount } from 'svelte';
+
+	let name = 'hello';
+	let isBrowser = false;
+
+	onMount(() => {
+		name = 'world';
+		isBrowser = true;
+	});
+</script>
+
+<h1>Welcome to {name}</h1>
+{browser} {isBrowser}
